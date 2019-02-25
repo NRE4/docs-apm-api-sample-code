4006d4eb6a83db3778c85bd4f58133b21cefb57c
gmalange <gmalange@appneta.com>
2019-02-25 13:30:17 -0800

    Updated to check for days_back and day_range == 0 and print different data depending on path type.

--- aggregation-test.py
+++ aggregation-test.py
@@ -6,21 +6,23 @@ import math
 from api_fns import *
 
 count = 0
-org_id = '11111'
-path_id = 22222
-
-days_back = 0    # the number of days ago the range ends
-day_range = 30   # the number of days in the range
-
 metric = 'availablecapacity'
-end = math.floor(time.time()-(60*60*24*days_back))
-start = (end - (60*60*24*day_range))
+
+org_id = '11111'  # organization ID
+path_id = 222222  # network path ID
+days_back = 0     # the number of days ago the range ends
+day_range = 30    # the number of days in the range
+
+if (days_back == 0) & (day_range == 0):
+	start = end = None
+else:
+	end = math.floor(time.time()-(60*60*24*days_back))
+	start = (end - (60*60*24*day_range))
 
 print('Start time: {} ({})'.format(time.ctime(start), start))
 print('End time:   {} ({})'.format(time.ctime(end), end))
 print('Org id:     ({})'.format(org_id))
 print('Path id:    ({})'.format(path_id))
-
 
 r1 = get_network_path_stats(org_id, start, end, metric)
 if r1.status_code == requests.codes.ok:
@@ -30,10 +32,18 @@ if r1.status_code == requests.codes.ok:
 
 		if network_path_stats['pathId'] == path_id:
 			#pp_json(network_path_stats)
-			for test in network_path_stats['data']['availableCapacity']:
-				count += 1
-				print('      Start time={}   Period={}  Available Capacity value={} count={}'.format(time.ctime(test['start']/1000), test['period'], math.floor(test['value']), count))
-				#pp_json(test)
+			if network_path_stats['data']['availableCapacity'] != []:
+				# Single-ended paths have data within 'data'
+				for test in network_path_stats['data']['availableCapacity']:
+					count += 1
+					print('      Single-ended path -> Start time={}   Period={}  Available Capacity value={} count={}'.format(time.ctime(test['start']/1000), test['period'], math.floor(test['value']), count))
+					#pp_json(test)
+			else:
+				# Dual-ended paths have data within 'dataInbound' and 'dataOutbound'
+				for test in network_path_stats['dataInbound']['availableCapacity']:
+					count += 1
+					print('      Dual-ended path   -> Start time={}   Period={}  Available Capacity value={} count={}'.format(time.ctime(test['start']/1000), test['period'], math.floor(test['value']), count))
+					#pp_json(test)
 
 elif r1.status_code == requests.codes.bad_request:
 	print_err_json(r1.json())
