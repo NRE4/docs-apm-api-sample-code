6c96416424088d09655ece630926abebbc109181
NRE4 <70978246+NRE4@users.noreply.github.com>
2020-09-08 16:35:28 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
 
-apm_server="<apm-server-url>" # for example, "app-01.pm.appneta.com"
-access_token="<access-token>" # see https://docs.appneta.com/api-access-tokens.html
+apm_server="<app-11.pm.appneta.com>" # for example, "app-01.pm.appneta.com"
+access_token="<65aeea586c554554b4301085c08bfb6e>" # see https://docs.appneta.com/api-access-tokens.html
